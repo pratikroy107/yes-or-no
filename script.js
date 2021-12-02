b96293df0d8215d6d8deb9622be70877355adc96
Pratik Roy <pratikroy.profile@gmail.com>
2021-12-02 22:38:31 +0530

    second commit

--- script.js
+++ script.js
@@ -13,11 +13,15 @@ yes[0].addEventListener('click', function () {
 //left = 10-60
 //right = 10-60
 //top = 0-30
-//let l = Math.floor((Math.random() * 10) + 1);
+
 no[0].addEventListener('click', function() {
-    no[0].style.marginLeft = "100px";
-    console.log(Math.floor((Math.random() * 60) + 10));
-    /*no[0].style.marginRight = "100px";
-    no[0].style.marginTop = "40px";
-    no[0].style.marginBottom = "40px";*/
+    let l = Math.floor((Math.random() * 60) + 10);
+    no[0].style.marginLeft = `${l}px`;
+    //console.log(Math.floor((Math.random() * 60) + 10));
+
+    let r = Math.floor((Math.random() * 60) + 10);
+    no[0].style.marginRight = `${r}px`;
+
+    let t = Math.floor((Math.random() * 30) + 1);
+    no[0].style.marginTop = `${t}px`;
 });
